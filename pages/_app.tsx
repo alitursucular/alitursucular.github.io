dd89d7c93edb50519f87ab8cb227f916d0fd394d
Ali Tursucular <alitursucular@gmail.com>
2023-05-22 18:31:13 +0100

    removed unused fonts

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,17 +2,10 @@ import React from "react";
 import type { AppProps } from "next/app";
 import { Hydrate, QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { Raleway, IBM_Plex_Sans, VT323 } from "next/font/google";
+import { VT323 } from "next/font/google";
 import "normalize.css";
 import "bootstrap/dist/css/bootstrap-grid.min.css";
 import "@/styles/globals.scss";
-
-const raleway = Raleway({ subsets: ["latin"] });
-
-const ibmSans = IBM_Plex_Sans({
-    weight: "700",
-    subsets: ["latin"]
-});
 
 const vt323 = VT323({
     weight: "400",
@@ -29,8 +22,6 @@ export default function MyApp({ Component, pageProps }: AppProps) {
             <Hydrate state={pageProps.dehydratedState}>
                 <style jsx global>{`
                     :root {
-                        --raleway-font: ${raleway.style.fontFamily};
-                        --ibmSans-font: ${ibmSans.style.fontFamily};
                         --vt323-font: ${vt323.style.fontFamily};
                     }
                 `}</style>
