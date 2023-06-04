736f595cfe80f745822820c4b404e0b8961331ee
Ali Tursucular <alitursucular@gmail.com>
2023-06-04 19:41:33 +0100

    fix nextjs build

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+    experimental: {
+        appDir: true
+    },
+    images: {
+        unoptimized: true
+    },
     output: "export"
 };
 
