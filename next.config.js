285a2f3cbebbd8e1b455f52148f38a4de7b6248e
Ali Tursucular <alitursucular@gmail.com>
2023-06-11 19:17:51 +0100

    upgrade nextjs version

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const nextConfig = {
     // experimental: {
     //     appDir: true
     // },
-    output: "export"
+    output: "export",
+    images: { unoptimized: true }
 };
 
 module.exports = nextConfig;
