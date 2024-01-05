aa907ce744e2d3156d3f2c6d3d85b0194f7b8e33
Ali Tursucular <alitursucular@gmail.com>
2024-01-05 18:46:34 +0000

    fix image rendering from repo markdown data

--- src/app/components/Readme/Readme.tsx
+++ src/app/components/Readme/Readme.tsx
@@ -1,8 +1,10 @@
 "use client";
 
 /* eslint-disable react/no-children-prop */
+import path from "path";
 import { Container, Row, Col } from "react-bootstrap";
 import { notFound } from "next/navigation";
+import Image from "next/image";
 import remarkGfm from "remark-gfm";
 import rehypeRaw from "rehype-raw";
 import Tag, { TagsWrapper } from "@/components/Tag";
@@ -38,6 +40,27 @@ const Readme: React.FC<{ repoName: string }> = ({ repoName }) => {
                             <div className={styles.readme_markdownStyles}>
                                 <ReactMarkdown
                                     children={repo?.readme.data ?? ""}
+                                    components={{
+                                        img({ node, src, alt }) {
+                                            if (src) {
+                                                const srcBaseName = path.basename(src);
+                                                const generatedImg = `https://raw.githubusercontent.com/alitursucular/${repoName}/master/public/images/${srcBaseName}`;
+
+                                                return (
+                                                    <Image
+                                                        src={generatedImg}
+                                                        alt={alt || ""}
+                                                        width={0}
+                                                        height={0}
+                                                        sizes="100vw"
+                                                        style={{ width: "100%", height: "auto" }}
+                                                    />
+                                                );
+                                            } else {
+                                                return null;
+                                            }
+                                        }
+                                    }}
                                     remarkPlugins={[remarkGfm]}
                                     rehypePlugins={[rehypeRaw]}
                                 />
